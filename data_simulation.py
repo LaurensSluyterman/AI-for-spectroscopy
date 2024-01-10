7b8bb31dd368f2a70440f3c9694c78fb114afeb4
LaurensSluyterman <Laurens.sluyterman@hotmail.com>
2024-01-10 14:31:36 +0100

    updated doc-string

--- data_simulation.py
+++ data_simulation.py
@@ -12,28 +12,20 @@ def resample_spectra(spectra_1, spectra_2=None, size=None):
     This function uses bootstrapping to get a newly sampled set of spectra.
     The goal is to use a set of background spectra and to resample this to
     get a new realisation of the noise. Additionally, we do this in such a way
-    to also have a background drift. This can be done in two ways:
+    to also have a background drift. This is done as follows.
 
-    1. Only a single set of spectra is given. Suppose this set consists of
-       300 spectra. The set is split in the middle into two halves of size 150.
-       Both halves are then used to create two newly sampled sets of background
-       spectra of size 300 (or any other specified size).
-    2. Two sets os spectra is given. From each set, we create a new set of
-       spectra of size 300. The idea is that, since we used different sets,
-       these two sets will exhibit a stronger background drift.
+    Two sets of spectra is given. From each set, we create a new set of
+    spectra of either the same size or the specified size.
 
     The goal of these spectra is to use the pair to create a realistic
     new absorbance spectrum to train a model on. By sampling in this way,
     we incorporate both the random noise effects and the background drift.
-    The first strategy incorporates a very modest amount of background drift and
-    the second a much stronger one depending on the specific pair of
-    background sets that is used.
 
     Arguments:
         spectra_1 (np.array): An array containing multiple subsequent measurements
-            of a spectrum. Each row corresponds to a different measurement.
-        spectra_2: Optional. If not given, strategy 1 is used. If given,
-            strategy 2 is used.
+            of a power spectrum. Each row corresponds to a different measurement.
+        spectra_2: (np.array). An array containing multiple subsequent measurements
+            of a power spectrum. Each row corresponds to a different measurement.
         size: The desired number of measurements of the sets of spectra. If not
             specified, the number will be the same as the number of measurements
             of the first spectrum.
@@ -44,13 +36,6 @@ def resample_spectra(spectra_1, spectra_2=None, size=None):
     """
     if size is None:
         size = np.shape(spectra_1)[0]
-    if spectra_2 is None:
-        half_point = int(size / 2)
-        first_half = spectra_1[0:half_point, :]
-        second_half = spectra_1[half_point:, :]
-        new_spectra_1 = resample(first_half, n_samples=size, replace=True)
-        new_spectra_2 = resample(second_half, n_samples=size, replace=True)
-    else:
         new_spectra_1 = resample(spectra_1, n_samples=size, replace=True)
         new_spectra_2 = resample(spectra_2, n_samples=size, replace=True)
     return np.array(new_spectra_1), np.array(new_spectra_2)
@@ -78,15 +63,14 @@ def get_absorption_spectrum(transmission_spectrum, spectra_1, spectra_2=None,
 
 
 def gen_data_set(N_samples, absorption_list, concentrations, background_list):
-    # todo: update this docstring to match new implementation
     """
     Create a simulated data set.
 
     This function creates N_samples absorbance spectra based on a list of
-    absorption spectra and corresponding concentrations and a list of backbround
+    absorption spectra and corresponding concentrations and a list of background
     intensity spectra.
 
-    Two background spectra are sampled at random. Subsequently, the
+    Two distinct background spectra are sampled at random. Subsequently, the
     get_absorption_spectrum is used to combine these two background spectra and
     the selected absorption spectrum to a new absorption spectrum. This
     spectrum contains background drift due to the combination of the two
@@ -106,11 +90,16 @@ def gen_data_set(N_samples, absorption_list, concentrations, background_list):
     covariates = np.zeros((N_samples, len(background_list[0][0])))
     targets = np.zeros((N_samples, len(concentrations[0])))
     for i in range(N_samples):
+        # Randomly select one of the concentrations
         j = randint(0, len(concentrations) - 1)
+
+        # Randomly select two background spectra
         indices = random.sample([i for i in range(len(background_list))], 2)
         spectra_1 = background_list[indices[0]]
         spectra_2 = background_list[indices[1]]
 
+        # Combine the selected background spectra and absorption/concentration
+        # to construct a new absorbance spectrum
         absorption = get_absorption_spectrum(1 - absorption_list[j],
                                              spectra_1=spectra_1,
                                              spectra_2=spectra_2)
